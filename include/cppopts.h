5d90c538d687fd1415bba2d6e594c58bbd52aa8e
Prateek Kumar <prateek@prateekkumar.in>
2018-10-18 21:40:39 +0530

    Reformat code, add basic CMakeLists.txt
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- include/cppopts.h
+++ include/cppopts.h
@@ -1,35 +1,39 @@
 #ifndef _CPPOPTS_H_
-#define _CPPOPTS_H_ 
+#define _CPPOPTS_H_
 
-#include <string>
 #include <map>
+#include <string>
 
 namespace cppopts {
-    class Option {
-    public:
-        Option();
-        Option(std::string key, char short_key, std::string desc, std::string value);
-        std::string getKey();
-        void setValue(std::string val);
-        std::string getString();
-        int getInt();
-    private:
-        std::string key;
-        std::string value;
-        std::string desc;
-        char short_key;
-    };
 
-    class Options {
-    public:
-        Option operator[](std::string key);
-        void parse(int argc, char **argv);
-        void addOption(std::string key, char short_key, std::string desc, std::string default_value = "");
-    private:
-        std::map<std::string, Option> options;
-        std::map<char, std::string> short_keys;
-        void parse(std::string opt);
-    };
-}
+template <typename T>
+class Option {
+ public:
+  Option();
+  Option(std::string key, char short_key, std::string desc, T value);
+  std::string getKey();
+  void setValue(T val);
+  T getValue();
+
+ private:
+  std::string key;
+  T value;
+  std::string desc;
+  char short_key;
+};
+
+class Options {
+ public:
+  Option<std::string> operator[](std::string key);
+  void parse(int argc, char **argv);
+  void addOption(std::string key, char short_key, std::string desc,
+                 std::string default_value = "");
+
+ private:
+  std::map<std::string, Option<std::string>> options;
+  std::map<char, std::string> short_keys;
+  void parse(std::string opt);
+};
+}  // namespace cppopts
 
 #endif
--- tests/main.cpp
+++ tests/main.cpp
@@ -2,11 +2,13 @@
 #include "cppopts.h"
 
 int main(int argc, char **argv) {
-    cppopts::Options opts;
-    opts.addOption("abc", 'a', "Desc", "val");
-    opts.addOption("abcd", 'b', "Desc1", "val1");
-    opts.parse(argc, argv);
-    std::cout << "abc" << " - " << opts["abc"].getString() << std::endl;
-    std::cout << "abcd" << " - " << opts["abcd"].getString() << std::endl;
-    return 0;
+  cppopts::Options opts;
+  opts.addOption("abc", 'a', "Desc", "val");
+  opts.addOption("abcd", 'b', "Desc1", "val1");
+  opts.parse(argc, argv);
+  std::cout << "abc"
+            << " - " << opts[std::string("abc")].getValue() << std::endl;
+  std::cout << "abcd"
+            << " - " << opts[std::string("abcd")].getValue() << std::endl;
+  return 0;
 }
